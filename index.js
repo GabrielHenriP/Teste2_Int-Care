2a059dad8eefc190d98e37ce3099a5bf9be0ead5
gabriel <soadhenri@gmail.com>
2021-01-06 20:58:28 -0300

    code refactoring

--- index.js
+++ index.js
@@ -7,19 +7,56 @@ const zip = new AdmZip();
  
 let dataBuffer = fs.readFileSync('Padrao_TISS_Componente_Organizacional__202012.pdf');
 
-pdf(dataBuffer).then( data => {
-    // array com todas as linhas extraidas do PDF
-    const arrayLinesFromPdf = data.text.split('\n').filter( phrase => {
-        return /[A-Za-z0-9]/.test(phrase)
-    }).map(phrase => phrase.trim())
 
-    let indexesToSlice = [];
-    arrayLinesFromPdf.forEach( (phrase, index) => {
-        if(phrase.indexOf("Quadro 30") !== -1 || phrase.indexOf("5 ANS") !== -1)
-            indexesToSlice.push(index)// guardando num array os indexes das linha "Quadro30" e "5 ANS"
+pdf(dataBuffer).then( data => {
+    // Obtendo array com todas as linhas extraidas do PDF após split,filter e trim
+    
+    return data.text
+        .split('\n')
+        .filter( phrase => /[A-Za-z0-9]/.test(phrase) )
+        .map(phrase => phrase.trim())
+    
+}).then( arrayLinesFromPdf => {
+    // Obtendo indexs de linhas específicas: inicio e fim de cada tabela
+    const indexesToSliceObject = getIndexes(arrayLinesFromPdf)
+
+    const tables = separateTables(arrayLinesFromPdf,indexesToSliceObject)
+
+    const dir = 'Teste_Intuitive_Care_Gabriel';
+
+    fs.mkdir(dir, (err) => {
+        console.log("Directory is created.");
+        saveCsvFiles(tables,dir);
     })
+})
+
+function getIndexes(arrayLinesFromPdf){
+    
+    let object = {
+        indexesTable1: [],
+        indexesTable2: [],
+        indexesTable3: [],
+    };
+    arrayLinesFromPdf.forEach( (phrase, index) => {
+        
+        if(phrase.indexOf("Quadro 30") !== -1 || phrase.indexOf("5 ANS") !== -1){
+            object['indexesTable1'].push(index)
+        }
+        if(phrase.indexOf("Quadro 31") !== -1 || phrase.indexOf("168 Guia de tratamento odontológico") !== -1){
+            object['indexesTable2'].push(index)
+        }
+        if(phrase.indexOf("3 Exclusão") !== -1){
+            object['indexesTable3'].push(index)
+        }
+    })
+    return object
+}
+
+function separateTables(arrayLinesFromPdf,indexesToSliceObject){
     // tratanto cada linha e guardando em forma de objeto
-    const arrayTable_1 = arrayLinesFromPdf.slice(indexesToSlice[0]+4, indexesToSlice[1]+1)
+    
+    const arrayTable_1 = arrayLinesFromPdf
+        .slice(indexesToSliceObject.indexesTable1[0]+4, indexesToSliceObject.indexesTable1[1]+1)
         .map( phrase => {
             let data = {
                 'Código': phrase.split(" ").slice(0,1).join(" "),
@@ -28,13 +65,9 @@ pdf(dataBuffer).then( data => {
             return data
         })
     
-    indexesToSlice = [];
-    arrayLinesFromPdf.forEach( (phrase, index) => {
-        if(phrase.indexOf("Quadro 31") !== -1 || phrase.indexOf("168 Guia de tratamento odontológico") !== -1)
-            indexesToSlice.push(index)// guardando num array os indexes das linha "Quadro 31" e "168 Guia de tratamento odontológico"  
-    })
     // tratanto cada linha e guardando em forma de objeto
-    const arrayTable_2 = arrayLinesFromPdf.slice(indexesToSlice[0]+3, indexesToSlice[1]+1)
+    const arrayTable_2 = arrayLinesFromPdf
+        .slice(indexesToSliceObject.indexesTable2[0]+3, indexesToSliceObject.indexesTable2[1]+1)
         .filter( phrase => {
             return phrase.length > 2 && phrase !== 'Padrão TISS - Componente Organizacional – dezembro de 2020'
         })
@@ -61,14 +94,9 @@ pdf(dataBuffer).then( data => {
             return Object.keys(object).length !== 0
         })
 
-    indexesToSlice = [];
-    arrayLinesFromPdf.forEach( (phrase, index) => {
-        phrase.trim()
-        if(phrase.indexOf("3 Exclusão") !== -1)
-            indexesToSlice.push(index)// guardando o index da linha "3 Exclusão" 
-    })
     // tratanto cada linha e guardando em forma de objeto
-    const arrayTable_3 = arrayLinesFromPdf.slice(indexesToSlice[0]-2, indexesToSlice[0]+1)
+    const arrayTable_3 = arrayLinesFromPdf
+        .slice(indexesToSliceObject.indexesTable3[0]-2, indexesToSliceObject.indexesTable3[0]+1)
         .map( phrase => {
             let data = {
                 'Código': phrase.split(" ").slice(0,1).join(" "),
@@ -78,18 +106,11 @@ pdf(dataBuffer).then( data => {
         })
 
     // array tables recebe 3 arrays de objetos, cada um representando uma tabela do PDF
-    const tables = [arrayTable_1, arrayTable_2, arrayTable_3];
-    const dir = './Teste_Intuitive_Care_Gabriel';
-    fs.mkdir(dir, (err) => {
-        console.log("Directory is created.");
-        saveCsvFiles(tables,dir);
-    })
-    
-
-})
+    return [arrayTable_1, arrayTable_2, arrayTable_3];
+     
+}
 
 function saveCsvFiles(tables,dir){
-    
     fs.access(dir, function(error) {
         if (error) {
             console.log('directory does not exists')
@@ -101,11 +122,11 @@ function saveCsvFiles(tables,dir){
                 
                 (async () => {
                     const csv = new ObjectsToCsv(table);
-                    await csv.toDisk(`${dir}/Quadro_${numberTable}.csv`);
+                    await csv.toDisk('./'+dir+`/Quadro_${numberTable}.csv`);
                 })().then( () => {
                     count++
                     if(count == 3){
-                        zipFolder();
+                        zipFolder(dir);
                     }
                        
                 });
@@ -120,10 +141,8 @@ function saveCsvFiles(tables,dir){
     
 }
 
-function zipFolder(){
+function zipFolder(dir){
     // zipando a pasta que contém os arquivos csv
-    zip.addLocalFolder(__dirname+'/Teste_Intuitive_Care_Gabriel')  
-    zip.writeZip(__dirname+'/Teste_Intuitive_Care_Gabriel.zip')
+    zip.addLocalFolder(__dirname+`/${dir}`)  
+    zip.writeZip(__dirname+`/${dir}.zip`)
 }
-
-
